b3c3dee5d750d1384ef12a0dfb00a4e960fb1fc7
vikas-borate <50566970+vikas-borate@users.noreply.github.com>
2022-05-03 22:22:24 +0530

    Update CountLines.bat

--- CountLines.bat
+++ CountLines.bat
@@ -1,16 +1,18 @@
 @echo off
 
-REAM - This script creates a report with filenames and number of lines of each file in all subfolders of current folder
-
 REM - Created by Vikas Borate
 
-echo.> Count_Lines_Report.txt
+echo.> Count_Lines_Report.csv
 
-FOR /F %%A IN ('DIR /B /S *.txt') do (
+ECHO "Report Process Started"
 
-FOR /F %%B IN ('findstr /R /N "^" %%A ^| find /C ":"') DO (
+FOR /F "delims=" %%A IN ('DIR /B /S *.*') do (
 
-ECHO %%~nxA,%%~dpA,%%B >>Count_Lines_Report.txt
+FOR /F %%B IN ('findstr /R /N "^" ^"%%A^" ^| find /C ":"') DO (
+
+ECHO %%~nxA,%%~dpA,%%B >>Count_Lines_Report.csv
 
 )
 )
+
+ECHO "Report Generated"
